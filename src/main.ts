cccec16277a3127bde6fb5ea28ebf86891e7529e
Anton <bogachuk566@gmail.com>
2024-02-02 12:52:12 +0200

    refactor: set global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('rowdy-api');
+
   await app.listen(4200);
 }
 bootstrap();
